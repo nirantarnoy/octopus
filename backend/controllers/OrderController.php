3b2a7a074a27344c8bcf7b75a30e3c988411e5c0
tarlektarnoy <niran_tarlek@hotmail.com>
2019-01-03 11:55:30 +0700

    update

--- backend/controllers/OrderController.php
+++ backend/controllers/OrderController.php
@@ -371,8 +371,8 @@ class OrderController extends Controller
         if($picid){
             $model = \common\models\OrderFile::find()->where(['id'=>$picid])->one();
             if($model){
-                unlink(Yii::getAlias('@backend') .'/web/uploads/thumpnail/'.$model->name);
-                unlink(Yii::getAlias('@backend') .'/web/uploads/images/'.$model->name);
+                //unlink(Yii::getAlias('@backend') .'/web/uploads/thumpnail/'.$model->name);
+               //unlink(Yii::getAlias('@backend') .'/web/uploads/images/'.$model->name);
                 \common\models\OrderFile::deleteAll(['id'=>$picid]);
             }
 
